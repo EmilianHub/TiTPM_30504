4cdddd0c8c50391fd590b630d716c60973e5d937
Emilian Kochanek <alone.wolfly@gmail.com>
2022-11-13 11:06:15 +0100

    05

---
+++ TiTPM_05/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Algorytm algorytm = new Algorytm();
+
+        algorytm.zapiszWiadomosc();
+        algorytm.zapiszDoPlikuONazwie();
+        algorytm.odczytajZPliku();
+        algorytm.decryptMessage();
+    }
+}
